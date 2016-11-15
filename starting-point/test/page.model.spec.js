d2b0b9cb5c28792aaefc3deae9311c01121d3a14
Garity Chapman <garitychapman@gmail.com>
2016-11-15 16:29:27 -0500

    working on findSimilar tests

--- starting-point/test/page.model.spec.js
+++ starting-point/test/page.model.spec.js
@@ -2,18 +2,21 @@ var chai = require('chai');
 var expect = chai.expect;
 var spies = require('chai-spies');
 chai.use(spies);
+chai.should();
+chai.use(require('chai-things'));
 var models = require('../models');
 var Page = models.Page;
+var Promise = require('bluebird');
 
 describe('testing page model', function(){
 
-	// beforeEach('Sync tables', function (done) {
- //        Page.sync({force: true})
- //            .then(function () {
- //                done();
- //            })
- //            .catch(done);
- //    });
+	beforeEach('Sync tables', function (done) {
+        Page.sync({force: true})
+            .then(function () {
+                done();
+            })
+            .catch(done);
+    });
 
  	// var page;
 
@@ -55,15 +58,100 @@ describe('testing page model', function(){
 	});
 
   	describe('Class methods', function () {
+
+  		beforeEach(function(done){
+  			Page.create({
+  				title: 'sql123',
+  				content: 'Database, more content...',
+  				tags: ['programming', 'coding', 'computers']
+  			})
+  			.then(function(){
+  				done();
+  			})
+  			.catch(done);
+  		});
+
 	    describe('findByTag', function () {
-	      it('gets pages with the search tag');
+	      it('gets pages with the search tag', function(){
+	      	Page.findByTag('coding')
+	      	.then(function(pages){
+	      		expect(pages).to.have.lengthOf(1);
+	      	})
+	      	// .then(function(){
+	      	// 	done();
+	      	// })
+	      });
 	      it('does not get pages without the search tag');
 	    });
   	});
   	describe('Instance methods', function () {
+  		// var page1;
+  		// 	page2,
+  		// 	page3;
+
+  		beforeEach(function(done){
+  			// [page1, page2, page3]
+  			
+  			Promise.all([
+  				Page.create({
+  				title: 'python',
+  				content: 'stuff about python, and more content...',
+  				tags: ['programming', 'coding', 'computers']
+	  			}),
+	  			Page.create({
+	  				title: 'sql123',
+	  				content: 'Database, more content...',
+	  				tags: ['programming']
+	  			}),
+	  			Page.create({
+	  				title: 'All about dogs',
+	  				content: 'this has nothing to do with computers',
+	  				tags: ['puppy', 'ears', 'tail']
+	  			}),
+	  			Page.create({
+	  				title: 'typing',
+	  				content: 'Mavis Beacon teaching typing class',
+	  				tags: ['computers']
+	  			})
+  			])
+  			.then(function(){
+  				done();
+  			})
+  			.catch(done);
+  		})
 	    describe('findSimilar', function () {
-	      it('never gets itself');
-	      it('gets other pages with any common tags');
+	      it('never gets itself', function(done){
+	      	Page.findOne({
+	      		where: {
+	      			id: 1
+	      		}
+	      	})
+	      	.then(function(page){
+	      		return page.findSimilar();
+	      	})
+	      	.then(function(pages){
+	      		expect(pages.id).should.not.include({id: 1});
+	      	})
+	      	.then(function(){
+	      		done();
+	      	})
+	      	.catch(done);
+	      	
+	      });
+	      it('gets other pages with any common tags', function(){
+	      	Page.findOne({
+	      		where: {
+	      			id: 2
+	      		}
+	      	})
+	      	.then(function(page){
+	      		return page.findSimilar();
+	      	})
+	      	.then(function(pages){
+	      		console.log(pages);
+	      		// expect(pages.id).should.include.
+	      	})
+	      });
 	      it('does not get other pages without any common tags');
 	    });
   	});
